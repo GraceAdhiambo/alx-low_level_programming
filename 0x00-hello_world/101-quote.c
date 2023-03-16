709c2a36738fe90db35c5121e75823e6b0a5a051
GraceAdhiambo <odhiambograce98@gmail.com>
2023-03-16 11:49:35 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+  * main - this is the main function
+  *
+  *Return: return 1 (successful)
+  */
+int main(void)
+{
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
