1bb1733c4f0f1951e6392ba08ed451155d1bb78e
GraceAdhiambo <odhiambograce98@gmail.com>
2023-03-21 05:51:40 -0700

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -4,15 +4,14 @@
   * @a: parameter
   * Return: always b
   */
-int print_last_digit(int a)
+int print_last_digit(int i)
 {
-	int b;
+	int k;
 
-	b = a % 10;
-	if (a < 0)
-	{
-		b = -a;
-	}
-	_putchar(b + '0');
-	return (b);
+	k = i % 10;
+
+	if (i < 0)
+		k = -k;
+	_putchar(k + '0');
+	return (k);
 }
