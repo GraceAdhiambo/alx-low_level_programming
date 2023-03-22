cc1f0f661f6c633ff87119cf93c0ed40ff6ca522
GraceAdhiambo <odhiambograce98@gmail.com>
2023-03-22 02:02:19 -0700

    fibonacci even sum

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  * main - prints sum of even numbers
+  * Return: always 0
+  */
+int main(void)
+{
+	int i = 1, j = 1, sum = 0;
+
+	while (i < 4000000)
+	{
+		i = i + j;
+		j = i - j;
+		if (i % 2 == 0)
+			sum = sum + i;
+	}
+	printf("Sum is: %d\n", sum);
+	return (0);
+}
