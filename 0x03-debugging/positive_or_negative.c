726242462ddd1bab91052a857536e05912e310bb
GraceAdhiambo <odhiambograce98@gmail.com>
2023-03-23 03:02:47 -0700

    tesr for positivr or negative number

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+  * positive_or_negative - checks if number is positive or negative
+  * @i: parameter to be checked
+  * Return: value
+  */
+void positive_or_negative(int i)
+{
+	if (i >= 0)
+		printf("%d is positive\n", i);
+	else
+		printf("%d is negative\n", i);
+}
