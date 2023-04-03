ae196c904c0b8ca42856faf8256bc726a8af74ca
GraceAdhiambo <odhiambograce98@gmail.com>
2023-04-03 11:50:35 -0700

    strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -8,7 +8,7 @@
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int i = 0;
-	int j;
+	unsigned int j;
 
 	while (*s)
 	{
@@ -16,13 +16,11 @@ unsigned int _strspn(char *s, char *accept)
 		{
 			if (*s == accept[j])
 			{
-				 i++;
-			break;
+				i++;
+				break;
 			}
-			else if (accept[j] + 1 == '\0')
-			{
+			else if (accept[j + 1] == '\0')
 				return (i);
-			}
 		}
 		s++;
 	}
