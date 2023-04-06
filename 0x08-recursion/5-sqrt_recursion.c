59354c9e308feb02025db5f1984d547a7c5f6ebe
GraceAdhiambo <odhiambograce98@gmail.com>
2023-04-06 07:04:13 -0700

    prime numbers

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,7 +1,27 @@
 #include "main.h"
 /**
-  *
+  * _sqrt_recursion - returns natural squareroot
+  * @n: num parameter
+  * Return: -1 if n has no natural squareroot
   */
 int _sqrt_recursion(int n)
 {
-	
+	if (n < 0)
+		return (-1);
+	return (_asqrt_recursion(n, 0));
+}
+
+/**
+  *_asqrt_recursion - calculates squareroot of number
+  * @n: number whose squareroot we're calculating
+  * @i: num
+  * Return: squareroot
+  */
+int _asqrt_recursion(int n, int i)
+{
+	if (i * i > n)
+		return (-1);
+	if (i * i == n)
+		return (i);
+	return (_asqrt_recursion(n, i + 1));
+}
--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,8 +1,27 @@
 #include "main.h"
 /**
-  *
+  *is_prime_number - checks if number is prime number
+  * @n: number
+  * Return: 1 if prime number 0 otherwise
   */
 int is_prime_number(int n)
 {
+	if (n <= 1)
+		return (0);
+	return (_prime_number(n, n - 1));
+}
 
+/**
+  * _prime_number - calculates if number is prime
+  * @n: num 1
+  * @i: num
+  * Return: 1 if n is prime number 0 otherwise
+  */
+int _prime_number(int n, int i)
+{
+	if (i == 1)
+		return (1);
+	if (n % i == 0 && i > 0)
+		return (0);
+	return (_prime_number(n, i - 1));
 }
