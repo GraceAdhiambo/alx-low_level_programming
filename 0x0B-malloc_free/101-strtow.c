77436c0f245812f761b0e049bd9ec235a376deb4
GraceAdhiambo <odhiambograce98@gmail.com>
2023-04-12 15:27:18 -0700

    strtow

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,50 +1,68 @@
-#include "main.h"
 #include <stdlib.h>
+#include "main.h"
 /**
-  * strtow - split string imto words
-  * @str: string
-  * Return: pointer if successfull and null otherwise
-  */
+ * word_count - counts number of words in a string
+ * @s: string to evaluate
+ * Return: number of words
+ */
+int word_count(char *s)
+{
+	int j, i, w;
+
+	j = 0;
+	w = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == ' ')
+			j = 0;
+		else if (j == 0)
+		{
+			j = 1;
+			w++;
+		}
+	}
+	return (w);
+}
+/**
+ * **strtow - splits a string into words
+ * @str: string to split
+ * Return: pointer to strings (Success) or NULL (Error)
+ */
 char **strtow(char *str)
 {
-	int j, i = 0, count = 1;
-	char *p = str;
-	char **words;
+	char **matrix, *tmp;
+	int i, k = 0, len = 0, words, c = 0, start, end;
 
-	if (str == NULL || *str == '\0')
-	{
+	while (*(str + len))
+		len++;
+	words = word_count(str);
+	if (words == 0)
 		return (NULL);
-	}
-	while (*p != '\0')
-	{
-		if (*p == ' ')
-		{
-			count++;
-		}
-		p++;
-	}
-	words = malloc((count + 1) * sizeof(char *));
-	if (words == NULL)
-	{
+	matrix = (char **) malloc(sizeof(char *) * (words + 1));
+	if (matrix == NULL)
 		return (NULL);
-	}
-	p = strtok(str, " ");
-	while (p != NULL)
+	for (i = 0; i <= len; i++)
 	{
-		words[i] = malloc(strlen(p) + 1);
-		if (words[i] == NULL)
+		if (str[i] == ' ' || str[i] == '\0')
 		{
-			for (j = 0; j < i; j++)
+			if (c)
 			{
-				free(words[j]);
+				end = i;
+				tmp = (char *) malloc(sizeof(char) * (c + 1));
+				if (tmp == NULL)
+					return (NULL);
+				while (start < end)
+					*tmp++ = str[start++];
+				*tmp = '\0';
+				matrix[k] = tmp - c;
+				k++;
+				c = 0;
 			}
-			free(words);
-			return (NULL);
 		}
-		strcpy(words[i], p);
-		i++;
-		p = strtok(NULL, " ");
+		else if (c++ == 0)
+			start = i;
 	}
-	words[count] = NULL;
-	return (words);
+	matrix[k] = NULL;
+	return (matrix);
 }
