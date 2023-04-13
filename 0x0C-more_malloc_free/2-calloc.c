22daaa8a277aeb5d97d8e30bdb5563e4888b9cf7
GraceAdhiambo <odhiambograce98@gmail.com>
2023-04-13 13:19:36 -0700

    hotfix

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -25,6 +25,7 @@ char *_memset(char *s, char b, unsigned int n)
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *memloc;
+
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	memloc = malloc(size * nmemb);
