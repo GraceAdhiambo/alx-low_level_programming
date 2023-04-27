0f17753f2e32bd3486ba11963c36a096fad5da16
GraceAdhiambo <odhiambograce98@gmail.com>
2023-04-27 06:01:00 -0700

    printlist

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+  *
+  */
+size_t print_list(const list_t *h)
+{
+	size_t n = 0;
+	while (h)
+	{
+	if (h->str == NULL)
+		printf("[0] (nil)");
+	else
+		printf("[%u] %s\n", h->len, h->str);
+	h = h->next;
+
+	n++;
+	}
+	return (n);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,3 @@
+0. Print list
+1. List length
+Add node
