27ab861254bd4e2ed04d1030f2cd79340b8a86ab
GraceAdhiambo <odhiambograce98@gmail.com>
2023-04-27 06:06:32 -0700

    fix

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include "lists.h"
 /**
-  *
+  * print_list - prints all elements of list_t
+  * @h: pointer to list_t
+  * Return: no.of nodes
   */
 size_t print_list(const list_t *h)
 {
 	size_t n = 0;
+
 	while (h)
 	{
 	if (h->str == NULL)
