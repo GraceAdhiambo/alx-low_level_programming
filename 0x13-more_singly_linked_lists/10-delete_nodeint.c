c4ebe680ba7d404e5efcd092432980e1874dfa23
GraceAdhiambo <odhiambograce98@gmail.com>
2023-05-02 11:37:40 -0700

    10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -27,7 +27,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		n++;
 	}
 	now = temp->next;
-	temp->next = current->next;
+	temp->next = now->next;
 	free(now);
 	return (1);
 }
